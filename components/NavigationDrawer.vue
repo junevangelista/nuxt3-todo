4768c323bf5f9a878a4ce23bb84388a1b5dd1e9c
Jun Evangelista <junevangelista@gmail.com>
2024-10-08 08:21:55 +0800

    Add the navigation drawer

---
+++ components/NavigationDrawer.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const links = [
+  ['mdi-inbox-arrow-down', 'To Do List'],
+]
+</script>
+
+<template>
+  <v-navigation-drawer>
+    <v-list>
+      <v-list-item v-for="[icon, text] in links" :key="icon" :prepend-icon="icon" :title="text" link></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
